b7fcb2d647cdf143e57f7a8ae90c1af5b3512345
Derga <dergan73@mail.ru>
2022-10-04 14:05:58 +0400

    hello world

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello world
+        
         </a>
       </header>
     </div>
