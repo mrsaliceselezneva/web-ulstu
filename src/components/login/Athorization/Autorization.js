23377d7ffb7f7d4ffc7d6062626de8efbba4ae5a
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2022-10-12 16:28:42 +0400

    выделенние поля ввода синим. скрыть/показать пароль. красивая кнопка войти

--- src/components/login/Athorization/Autorization.js
+++ src/components/login/Athorization/Autorization.js
@@ -16,6 +16,8 @@ function Autorization() {
 
   return (
       <div className='autinfication'>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" />
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
         <div className='text-logo'>
           <img src={logoUlstu} alt="альтернативный текст" />
           <div>Learn.Ulstu</div>
@@ -24,17 +26,27 @@ function Autorization() {
         <div className='text-info'>Веб-приложение для автоматизации обучения в УлГТУ</div>
         <div className='login'>
           <div className='text-title'>Авторизация</div>
-            <div className='email'>
+            {/* <div className='email'>
               <img src={emailImg} alt='email' className='img'/>
               <input placeholder='email@ulstu.ru'/>
+            </div> */}
+            <div className='input-block'>
+              <input className="input" type="email" placeholder='email@ulstu.ru'/>      
+              <i className='fa fa-envelope login-icon' />
             </div>
-            <input type={show?"text":"password"} placeholder='пароль' 
             
-            />
-            <label onClick={passwordShow} className={show?"password_show":"password"}></label>
+            <div className='input-block'>
+              <input className="input" type={show?"text":"password"} placeholder='пароль'/>      
+              <i onClick={passwordShow} className={show?'fa fa-eye-slash login-icon':'fa fa-eye login-icon'} />
+            </div>
+            
             <button onClick={Clack}>Войти</button>
             <div className='text-bottom'>Нет аккаунта?<a href='#' className='href'>Создать</a></div>
         </div>
+
+        {/* <div className='question'>
+            <button className='question'></button>
+        </div> */}
       </div>
   );
 }
