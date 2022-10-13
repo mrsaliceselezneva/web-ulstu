bcc90ac9b18f6f32c5b955d3a956cc0d551b8826
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2022-10-13 16:13:34 +0400

    добавляен слушатель нажатия enter в области ввода. попытка отправить get запрос на проверку введённых данных

--- src/components/login/Athorization/Autorization.js
+++ src/components/login/Athorization/Autorization.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import axios from "axios";
+import React, {useEffect, useState} from 'react';
 import logoUlstu from '../../assets/images/logo-ulstu.png';
 import emailImg from '../../assets/images/email.png';
 
@@ -6,16 +7,31 @@ import '../Login.scss';
 
 function Autorization() {
   const [show, setShow] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const passwordShow=()=>{
     setShow(!show);
   }
 
-  function Clack(){
-    console.log(300);
-  }
+  function Check(){
+    console.log(email);
+    console.log(password);
+    axios
+      .get(`http://asus.russianitgroup.ru/api/login${email}/${password}`)
+      .then((response) => {
+        console.log(response.data);
+      });
+  };
+
+
 
   return (
-      <div className='autinfication'>
+      <div onKeyDown={event => {
+          if (event.key == "Enter")
+            Check();
+          }} 
+          className='autinfication'>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" />
         <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
         <div className='text-logo'>
@@ -31,16 +47,22 @@ function Autorization() {
               <input placeholder='email@ulstu.ru'/>
             </div> */}
             <div className='input-block'>
-              <input className="input" type="email" placeholder='email@ulstu.ru'/>      
+              <input 
+                onChange={(event) => setEmail(event.target.value)} 
+                className="input" type="email" placeholder='email@ulstu.ru'
+              />      
               <i className='fa fa-envelope login-icon' />
             </div>
             
             <div className='input-block'>
-              <input className="input" type={show?"text":"password"} placeholder='пароль'/>      
+              <input 
+                onChange={(event) => setPassword(event.target.value)}
+                className="input" type={show?"text":"password"} placeholder='пароль'
+              />      
               <i onClick={passwordShow} className={show?'fa fa-eye-slash login-icon':'fa fa-eye login-icon'} />
             </div>
             
-            <button onClick={Clack}>Войти</button>
+            <button onClick={Check}>Войти</button>
             <div className='text-bottom'>Нет аккаунта?<a href='#' className='href'>Создать</a></div>
         </div>
 
