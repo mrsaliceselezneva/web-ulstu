8a01843d71565fbcad31f0caa4b074578fe1cf0d
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2022-10-22 15:22:41 +0400

    макс, запуш апдейт

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -5,34 +5,33 @@ import { FiBarChart2, FiCalendar, FiCheckSquare, FiEdit } from 'react-icons/fi';
 import { useState } from 'react';
 import repeatBackground from '../assets/images/repeat-background.png';
 
-
-const Drawer = ({ children }) => {
+const Drawer = ({children}) => {
 
     const [isOpen, setIsOpen] = useState(false)
 
     const toggle = () => setIsOpen(!isOpen)
 
     const routes = [
-        {
-            path: "/",
-            name: "Главаня",
-            icon: <FiBarChart2 />
-        },
-        {
-            path: "/timetable",
-            name: "Расписание",
-            icon: <FiCalendar />
-        },
-        {
-            path: "/subjects",
-            name: "Предметы",
-            icon: <FiCheckSquare />
-        },
-        {
-            path: "/login",
-            name: "Ответы преподавателей",
-            icon: <FiEdit />
-        }
+       {
+        path: "/",
+        name:"Главаня",
+        icon: <FiBarChart2/>
+       },
+       {
+        path: "/timetable",
+        name:"Расписание",
+        icon: <FiCalendar/>
+       },
+       {
+        path: "/subjects",
+        name:"Предметы",
+        icon: <FiCheckSquare/>
+       },
+       {
+        path: "/login",
+        name:"Ответы преподавателей",
+        icon:<FiEdit/>
+       }
     ]
     
     return (
@@ -50,34 +49,32 @@ const Drawer = ({ children }) => {
                      
                         <img src='/images/logo.svg' alt = "logo" />
                     
-=======
-
                     <div className="drawer_span">
                         {isOpen && <p className="logo"><b>Learn</b>.Ulstu</p>}
                     </div>
 
                 </div>
 
-                <section className="routes">
+                <section className="routes"> 
 
                     {routes.map((route) => (
-
-                        <NavLink to={route.path} key={route.name} className="link">
-
-                            <div className="icon">{route.icon}</div>
-
-                            {isOpen && <motion.div className="link_text">{route.name}</motion.div>}
+                        
+                        <NavLink to={route.path} key={route.name}className="link">
+                           
+                           <div className="icon">{route.icon}</div>
+                           
+                               {isOpen && <motion.div className="link_text">{route.name}</motion.div>}
                         </NavLink>
-
+        
                     ))}
 
                 </section>
-            </motion.div>
+            </motion.div> 
 
             <motion.div className={`children ${isOpen ? "children_opened" : ""}`}>
                 <main>{children}</main>
             </motion.div>
-        </div >
+        </div> 
     )
 }
 
