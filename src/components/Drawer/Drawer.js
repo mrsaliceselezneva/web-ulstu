35cb888d7bd4c030b84b6675fcc218f6e8fbcd9c
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2022-10-24 10:34:57 +0400

    новый sidebar и top-section

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,21 +1,21 @@
 import './drawer.scss';
 import {AnimatePresence, motion} from 'framer-motion';
 import { NavLink } from 'react-router-dom';
-import { FiBarChart2, FiCalendar, FiCheckSquare, FiEdit } from 'react-icons/fi';
+import { FiLogOut, FiCalendar, FiCheckSquare, FiEdit, FiHome, FiMessageSquare, FiBell } from 'react-icons/fi';
 import { useState } from 'react';
 import repeatBackground from '../assets/images/repeat-background.png';
 
-const Drawer = ({children}) => {
+const Drawer = () => {
+    const [open, setOpen] = useState(true);
 
-    const [isOpen, setIsOpen] = useState(false)
-
-    const toggle = () => setIsOpen(!isOpen)
+    // const toggle = () => setIsOpen(!isOpen);
+    const sidebarOpen = () => {setOpen(!open);};
 
     const routes = [
        {
         path: "/",
         name:"Главаня",
-        icon: <FiBarChart2/>
+        icon: <FiHome/>
        },
        {
         path: "/timetable",
@@ -29,10 +29,10 @@ const Drawer = ({children}) => {
        },
        {
         path: "/login",
-        name:"Ответы преподавателей",
-        icon:<FiEdit/>
+        name:"Чаты",
+        icon:<FiMessageSquare/>
        }
-    ]
+    ];
     
     return (
         <div className="container">
@@ -41,40 +41,59 @@ const Drawer = ({children}) => {
                 background-image: url(${repeatBackground}); 
                 background-size: contain;}`}
             </style>
-            <motion.div 
-                className={`sidebar ${isOpen ? "" : "opened"}`}>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" />
+            <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
 
-                <div className="top_section" onClick={toggle}>
-                     
-                        <img src='/images/logo.svg' alt = "logo" />
-                    
-                    <div className="drawer_span">
-                        {isOpen && <p className="logo"><b>Learn</b>.Ulstu</p>}
+            <div className='top-section'>
+                <a href='/' className='logo'>
+                    <img className='logo-img' src='/images/logo.svg' alt="logo" />
+                    <p>learn.UlSTU</p>
+                </a>
+                <div className='profile'>
+                    <div className='short-info'>
+                        <div className='name'>
+                            Имя Фамилия
+                        </div>
+                        <div className='group'>
+                            ИВТАСбд-41
+                        </div>
                     </div>
-
+                    <img className='avatar' src='./images/avatar.png' alt = "avatar" />
+                    <div className='notice-exit'>
+                        {/* <i className='fa fa-bell notice' aria-hidden="true"/> */}
+                        <FiBell className='notice' />
+                        <FiLogOut className='exit' />
+                    </div>
                 </div>
+            </div>
+            {/* <div>
+            <button onClick={sidebarOpen} 
+            className={`${open ? "fa fa-angle-double-left" : "fa fa-angle-double-right"} sidebar-button`}
+             />
+            <div className={`sidebar-${open ? "open" : "close"}`}>
 
-                <section className="routes"> 
-
-                    {routes.map((route) => (
+                {routes.map((route) => (
+                    <NavLink to={route.path} key={route.name} className="link">
                         
-                        <NavLink to={route.path} key={route.name}className="link">
-                           
-                           <div className="icon">{route.icon}</div>
-                           
-                               {isOpen && <motion.div className="link_text">{route.name}</motion.div>}
-                        </NavLink>
+                        <div className="icon">{route.icon}</div>
+                        
+                            {open && <div className="link_text">{route.name}</div>}
+                    </NavLink>
         
-                    ))}
+                ))}
+            </div> */}
+             <div className={`sidebar`}>
 
-                </section>
-            </motion.div> 
+                {routes.map((route) => (
+                    <NavLink to={route.path} key={route.name} className="link">
+                        <div className="icon">{route.icon}</div>
+                        <div className="link_text">{route.name}</div>
+                    </NavLink>
 
-            <motion.div className={`children ${isOpen ? "children_opened" : ""}`}>
-                <main>{children}</main>
-            </motion.div>
+                ))}
+            </div>
         </div> 
     )
-}
+};
 
 export default Drawer;
