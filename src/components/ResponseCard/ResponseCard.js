98f5b2515a53df8cd5ae10e2e2e30b40cd933849
Derga <dergan73@mail.ru>
2022-12-07 15:55:08 +0400

    добавил отклики

--- src/components/ResponseCard/ResponseCard.js
+++ src/components/ResponseCard/ResponseCard.js
@@ -1,8 +1,41 @@
 import "./ResponseCard.scss"
+import Time from '../../components/Time/Time'
 
-const ResponseCard = () => {
+const ResponseCard = ({ comment, project, user, date }) => {
+
+
     return (
-        <div>hello</div>
+        <div className="response__card__container">
+
+            <div className="response__card__avatar">
+                <img src={user.avatarId} />
+            </div>
+
+            <div className="response__card__user">
+                <span>{user.firstName} {user.lastName}</span>
+            </div>
+
+            <div className="response__card__project">
+                <span> {project.name}</span>
+            </div>
+
+            <div className="response__card__message">
+                <span>{comment}</span>
+            </div>
+
+            <div className="response__card__accept">
+                <span>Принять</span>
+            </div>
+
+            <div className="response__card__reject">
+                <span>Отклонить</span>
+            </div>
+
+            <div className="response__card__date">
+                <span>{date}</span>
+            </div>
+
+        </div>
     )
 }
 
--- src/pages/ResponseList/ResponseList.js
+++ src/pages/ResponseList/ResponseList.js
@@ -1,7 +1,45 @@
 import ResponseCard from "../../components/ResponseCard/ResponseCard"
 import './ResponseList.scss'
+import img from "../../components/assets/images/avatar.png"
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux";
+import axios from "axios"
 
 const ResponseList = () => {
+
+    const data = [
+        {
+            name: "Максим",
+            surname: "Дергунов",
+            avatar: img,
+            project: "Умный велосипедУмный велосипедУмный велосипедУмный велосипед",
+            message: "тестовое сообщениетестовое сообщениетестовое сообщениетестовое сообщениетестовое сообщениетестовое сообщение",
+            date: "06.12.22"
+
+        },
+        {
+            name: "Максим",
+            surname: "Дергунов",
+            avatar: img,
+            project: "Умный велосипедУмный велосипедУмный велосипедУмный велосипед",
+            message: "тестовое сообщениетестовое сообщениетестовое сообщениетестовое сообщениетестовое сообщениетестовое сообщение",
+            date: "06.12.22"
+
+        }
+    ]
+    const { token } = useSelector(state => state.userReducer);
+    const [response, setResponse] = useState([])
+
+    useEffect(() => {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+        axios
+            .get(`${process.env.REACT_APP_API_URL}/project/response/list`, { headers })
+            .then((response) => {
+                setResponse(response.data)
+            })
+    }, [])
 
     return (
         <div className="response__container">
@@ -10,7 +48,9 @@ const ResponseList = () => {
                 <h1>Список откликов</h1>
             </div>
 
-            <ResponseCard />
+            {response.map((item) => <ResponseCard
+
+                {...item} />)}
         </div>
     )
 }
